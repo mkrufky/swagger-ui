e278dc9094cb57f205223c31a181a20302039190
Bill Kaufman <bill@pagerduty.com>
2016-02-24 09:27:55 -0800

    [API-679] Add curl command to result dialog

--- src/main/javascript/view/OperationView.js
+++ src/main/javascript/view/OperationView.js
@@ -360,6 +360,8 @@ SwaggerUi.Views.OperationView = Backbone.View.extend({
         obj = this.model['do'](map, mockOpts);
         this.showHeaderParams(obj.headers);
 
+        this.showCurlCommand(this.model.asCurl(map, mockOpts));
+
         return this.model['do'](map, opts, this.showCompleteStatus, this.showErrorStatus, this);
       }
     }
@@ -442,6 +444,7 @@ SwaggerUi.Views.OperationView = Backbone.View.extend({
       complete: (function (_this) {
         return function (data) {
           _this.showHeaderParams(headerParams);
+          _this.showCurlCommand(null);
           return _this.showCompleteStatus(_this.wrap(data), _this);
         };
       })(this)
@@ -514,13 +517,24 @@ SwaggerUi.Views.OperationView = Backbone.View.extend({
     parent.showStatus(data);
   },
 
+  // show the curl command
+  showCurlCommand: function (curl) {
+    if (!curl) {
+      $('.curl_command, .curl_prompt', $(this.el)).hide();
+    } else {
+      $('.curl_command', $(this.el)).html('<pre></pre>');
+      $('.curl_command pre', $(this.el)).text(curl);
+      $('.curl_command, .curl_prompt', $(this.el)).show();
+    }
+  },
+
   // show the header params
   showHeaderParams: function (headerParams) {
     if ($.isEmptyObject(headerParams)) {
-      $('.request_headers, #request_headers_prompt', $(this.el)).hide();
+      $('.request_headers, .request_headers_prompt', $(this.el)).hide();
     } else {
       $('.request_headers', $(this.el)).html('<pre>' + _.escape(JSON.stringify(headerParams, null, '  ')).replace(/\n/g, '<br>') + '</pre>');
-      $('.request_headers, #request_headers_prompt', $(this.el)).show();
+      $('.request_headers, .request_headers_prompt', $(this.el)).show();
     }
   },
 
