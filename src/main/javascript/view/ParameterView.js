25584cb8902fa63c33cc7b7fc3f222479a19a9fd
Bill Kaufman <bill@pagerduty.com>
2016-02-23 17:05:29 -0800

    [API-672] Edit arrays as newline-separated textarea

--- src/main/javascript/view/ParameterView.js
+++ src/main/javascript/view/ParameterView.js
@@ -30,10 +30,12 @@ SwaggerUi.Views.ParameterView = Backbone.View.extend({
     this.model.paramType = this.model.in || this.model.paramType;
     this.model.isBody = this.model.paramType === 'body' || this.model.in === 'body';
     this.model.isFile = type && type.toLowerCase() === 'file';
-    this.model.default = (this.model.default || this.model.defaultValue);
+    this.model.isArray = (this.model.type.toLowerCase() === 'array' || this.model.allowMultiple);
+    this.model.default = (this.model.default || this.model.defaultValue) || '';
 
     if(this.model.format === 'password') {
         this.model.inputType = 'password';
+        this.model.isArray = false;
     } else {
         this.model.inputType = 'text';
     }
